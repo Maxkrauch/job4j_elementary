fed26efc698c24de6118f1a8ccac1357bd0a849d
Maxkrauch <kingsmax72@gmail.com>
2022-06-17 18:43:07 +0500

    6. Checkstyle [#4123]

--- src/main/java/ru/job4j/Studentinfo.java
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 public class Studentinfo {
-    public static void main(String[] agrs){
+    public static void main(String[] agrs) {
         System.out.println("Maksim Nechaev");
         System.out.println("13.09.1995");
     }

d040768300f9b9e8c5a3c70a9a0c34b623a47203
Maxkrauch <kingsmax72@gmail.com>
2022-06-17 19:46:34 +0500

    3. Арифметические операции. [#185]

--- src/main/java/ru/job4j/Studentinfo.java
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -3,6 +3,6 @@ package ru.job4j;
 public class Studentinfo {
     public static void main(String[] agrs) {
         System.out.println("Maksim Nechaev");
-        System.out.println("13.09.1995");
+        System.out.println("13.09.19954");
     }
 }

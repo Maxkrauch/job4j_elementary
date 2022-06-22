09b22d174667ce2d37dbb53b1b45be51ba769a24
Maxkrauch <kingsmax72@gmail.com>
2022-06-22 18:57:33 +0500

    9.1. String [#237407]

---
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -0,0 +1,12 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+     String idea = "I like Java!";
+     idea = idea + " But I am a newbie.";
+     System.out.println(idea);
+     int year = 2022;
+     System.out.println(year);
+    }
+}
+
